02623b0b9ab2dbfee9422e783c3ee0c108544c83
Toni <tonilee@berkeley.edu>
2016-11-16 13:02:06 -0800

    Added 3 Models, 3 Controllers, and a View

---
+++ EmailCollage/app/controllers/accounts_controller.rb
@@ -0,0 +1,11 @@
+class AccountsController < ApplicationController
+
+	def new 
+  	
+  	end
+ 
+
+  def create
+  	
+  end
+end
---
+++ EmailCollage/app/models/group.rb
@@ -0,0 +1,8 @@
+class Group < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :accounts
+  has_one :collage
+end
